cb0c2e0f151fe915866fb43b6f572cc7e72a1a56
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2022-07-26 11:43:38 +0530

    my commit

---
+++ linked_list/detect_cycle/flyod_algo.cpp
@@ -0,0 +1,90 @@
+// DETECTION OF CYCLE IN LINKED LIST
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+bool detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;       // one step
+        fast = fast->next->next; // two step
+
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startNode;
+    int count = 1;
+    while (temp->next != NULL) // this  ptr is traversing to the last
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    makeCycle(head, 3);
+    cout << detectCycle << endl;
+
+    display(head);
+
+    return 0;
+}
--- linked_list/reverseKnodes.cpp
+++ linked_list/reverseKnodes.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-
 class node
 {
 public:
     node *next;
     int data;
+
     node(int val)
     {
         data = val;
@@ -13,10 +13,18 @@ public:
     }
 };
 
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
 void insertAtTail(node *&head, int val)
 {
     node *n = new node(val);
     node *temp = head;
+
     if (head == NULL)
     {
         head = n;
@@ -27,19 +35,8 @@ void insertAtTail(node *&head, int val)
     {
         temp = temp->next;
     }
-    temp->next = n;
-}
 
-node *reverseRecursive(node *&head)
-{
-    if (head == NULL || head->next == NULL) // base case
-    {
-        return head;
-    }
-    node *newhead = reverseRecursive(head->next);
-    head->next->next = head;
-    head->next = NULL;
-    return newhead;
+    temp->next = n;
 }
 
 void display(node *head)
@@ -47,15 +44,39 @@ void display(node *head)
     node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->data << "->";
+        cout << temp->data << " ";
         temp = temp->next;
     }
     cout << "NULL" << endl;
 }
+// Function to reverse a node
+node *reverseK(node *&head, int k)
+{
+    node *prev = NULL;
+    node *curr = head;
+    node *nextptr;
+
+    int count = 0;
+    while (curr != NULL && count < k)
+    {
+        nextptr = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextptr;
+        count++;
+    }
+
+    if (nextptr != NULL)
+    {
+        head->next = reverseK(nextptr, k);
+    }
+
+    return prev;
+}
 
 int main()
 {
-
+    // node *head = NULL;
     node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
@@ -63,7 +84,9 @@ int main()
     insertAtTail(head, 4);
     insertAtTail(head, 5);
     display(head);
-    node *newhead = reverseRecursive(head);
+    int k = 2;
+    node *newhead = reverseK(head, k);
     display(newhead);
+
     return 0;
 }

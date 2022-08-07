3c10778c446b86ca101e51867d191a7f09e66248
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2022-08-07 14:44:11 +0530

    count all node

--- binaryTrees/buildtree/kth_sum.cpp
+++ binaryTrees/buildtree/kth_sum.cpp
@@ -13,37 +13,6 @@ struct Node
     }
 };
 
-void printLevelOrder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
-
-    while (!q.empty())
-    {
-        Node *node = q.front();
-        q.pop();
-        if (node != NULL)
-        {
-            cout << node->data << " ";
-            if (node->left)
-            {
-                q.push(node->left);
-            }
-            if (node->right)
-            {
-                q.push(node->right);
-            }
-        }
-        else if (!q.empty())
-            q.push(NULL);
-    }
-}
-
 int sumAtK(Node *root, int K)
 {
     if (root == NULL)
